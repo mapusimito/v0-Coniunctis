a75cb86fc9b81b613efd1460f1ad55c0d8de17ad
mapusimito <dagamapu@gmail.com>
2025-05-30 02:33:47 -0400

    Update footer.tsx

--- components/landing/footer.tsx
+++ components/landing/footer.tsx
@@ -104,7 +104,7 @@ export function Footer() {
 
 
         <div className="border-t border-gray-200 dark:border-gray-800 mt-12 pt-8 text-center">
-          <p className="text-gray-600 dark:text-gray-300">© 2025 Coniunctis.. </p>
+          <p className="text-gray-600 dark:text-gray-300">© 2025 Coniunctis. </p>
         </div>
       </div>
     </footer>
